41c5cf3603da63d3e2407ad480c791a5d3e455a7
lulorraine <milu1@student.unimelb.edu.au>
2022-06-04 05:51:12 +1000

    add js to checkout page

---
+++ CHECKOUT-PAGE/checkout-page.js
@@ -0,0 +1,12 @@
+function payPayment() {
+    var r=confirm("Do you want to continue to pay?");
+    if (r==true)
+    {
+        x="The payment is successful!";
+    }
+    else
+    {
+        x="The payment is cancelled.";
+        
+    }    
+}
---
+++ PRODUCT-LIST/product-list.js
@@ -0,0 +1,7 @@
+function addToCart() {
+    alert ("This album was successfully added to the cart!");
+}
+
+function addToWishlist() {
+    alert ("This album was successfully added to the wishlist!");
+}
